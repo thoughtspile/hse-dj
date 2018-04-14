9cda198efa00d97d092c8d338434e7f548b70595
Vladimir Klepov <v.klepov@gmail.com>
2018-04-14 14:02:38 +0300

    init lesson 7: web visualization

--- ref-project/ru_cities/spiders/hh.py
+++ ref-project/ru_cities/spiders/hh.py
@@ -6,13 +6,14 @@ city_names = [c['name'].lower() for c in json.load(open('./city-list-big-clean.j
 
 class HhSpider(scrapy.Spider):
     name = 'hh'
-    allowed_domains = ['hh.ru']
-    start_urls = ['https://hh.ru/area_switcher?from=region-clarification-dropdown&backUrl=/search/vacancy%3Farea%3D1%26only_with_salary%3Dtrue%26order_by%3Dpublication_time%26enable_snippets%3Dtrue%26clusters%3Dtrue']
+    allowed_domains = ['hh.ru', 'headhunter.ge']
+    start_urls = ['https://tbilisi.headhunter.ge/area_switcher?backUrl=/search/vacancy%3Farea%3D2758%26only_with_salary%3Dtrue%26order_by%3Dpublication_time%26enable_snippets%3Dtrue%26customDomain%3D1%26clusters%3Dtrue']
 
     def parse(self, response):
         for city in response.css('.area-switcher-city a'):
             name = city.css('::text').extract_first()
             if name.lower() in city_names:
+            # if name.lower() == 'екатеринбург':
                 yield response.follow(
                     url=city.css('::attr(href)').extract_first(),
                     meta={ 'city_name': name },
@@ -20,13 +21,40 @@ class HhSpider(scrapy.Spider):
 
     def parse_city(self, response):
         city_name = response.meta['city_name']
+        for spec in response.xpath('//*[@data-toggle="professionalArea"]/following-sibling::node()//li/a'):
+            yield response.follow(
+                url=spec.css('::attr(href)').extract_first(),
+                callback=self.parse_city_spec,
+                meta={
+                    'city_name': city_name,
+                    'spec': spec.css('.clusters-value__name::text').extract_first(),
+                    'spec_count': spec.css('.clusters-value__count::text').extract_first()
+                })
+
+
+    def parse_city_spec(self, response):
+        city_name = response.meta['city_name']
+        spec = response.meta['spec']
+        spec_count = response.meta['spec_count']
         for job in response.css('.vacancy-serp-item'):
             title = job.css('.vacancy-serp-item__title a::text').extract_first()
             salary = job.css('[itemprop=baseSalary]::attr(content)').extract_first()
-            yield { 'city_name': city_name, 'title': title, 'salary': salary }
+            salary_curr = job.css('[itemprop=salaryCurrency]::attr(content)').extract_first()
+            employer = job.css('[data-qa=vacancy-serp__vacancy-employer]::text').extract_first()
+            metro_station = job.css('.metro-station::text').extract_first()
+            yield {
+                'city_name': city_name,
+                'spec': spec,
+                'spec_count': spec_count,
+                'title': title,
+                'salary': salary,
+                'salary_curr': salary_curr,
+                'employer': employer,
+                'metro_station': metro_station
+            }
         next_page = response.css('[data-qa=pager-next]::attr(href)').extract_first()
         if next_page:
             yield response.follow(
                 url=next_page,
                 meta=response.meta,
-                callback=self.parse_city)
+                callback=self.parse_city_spec)
